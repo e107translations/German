320cc84f2976b1fb493ce54b627cd55c23b0f976
MaDDoG9x9 <65389831+MaDDoG9x9@users.noreply.github.com>
2020-05-16 11:04:00 +0200

    Add files via upload

--- e107_plugins/social/languages/German/German_global.php
+++ e107_plugins/social/languages/German/German_global.php
@@ -9,11 +9,9 @@
 |        $Author: yak $
 +---------------------------------------------------------------+
 */
+define("LAN_SOCIAL_ADMIN_SUMM", "Fügt Facebook, Twitter und andere Social-Media-Widgets zu e107 hinzu.");
 define("LAN_PLUGIN_SOCIAL_DESCR", "Fügt Optionen hinzu, die e107 Kommentar-Engine durch Facebook zu ersetzen.Fügen Sie Twitter Feeds zu Ihrer Website hinzu. usw.");
 define("LAN_PLUGIN_SOCIAL_SIGNIN", "Anmelden mit:");
 define("LAN_PLUGIN_SOCIAL_XUP_SIGNUP", "Anmelden mit deinen [x] Konto");
 define("LAN_PLUGIN_SOCIAL_XUP_REG", "Registrieren mit deinen [x] Konto");
 define("LAN_PLUGIN_SOCIAL_NAME", "Soziale");
-
-
-?>
