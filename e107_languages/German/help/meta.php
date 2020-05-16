96c3f96148d221d5f3acfcbf591ea3b3c2857dbf
MaDDoG9x9 <65389831+MaDDoG9x9@users.noreply.github.com>
2020-05-16 10:51:38 +0200

    Add files via upload

---
+++ e107_languages/German/help/meta.php
@@ -0,0 +1,21 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - german language file
+|     UTF-8 encoded
+|     translated for: http://www.e107cms.de
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $HeadURL: https://svn.code.sf.net/p/e107german/code/trunk/e107_0.7/e107_languages/German/admin/help/meta.php $ 
+|     $Revision: 115 $
+|     $Date: 2010-09-26 06:00:45 +0200 (So, 26. Sep 2010) $
+|     $Id: meta.php 115 2010-09-26 04:00:45Z lars78 $
+|     $Author: lars78 $
++----------------------------------------------------------------------------+
+*/
+$text = "Alle Meta-Tags, die Sie hier eingeben werden im Monitor am richtigen Platz angezeigt.";
+
+$ns -> tablerender("Meta Tags", $text);
+?>
