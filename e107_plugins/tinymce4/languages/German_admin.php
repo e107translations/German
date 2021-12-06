2c5d2d517cd4353f952307a910b8c88e5174ec34
leo <the_guardian2@hotmail.com>
2021-12-06 14:40:41 +0100

    v 2.3.1
    updated using machine translation

--- e107_plugins/tinymce4/languages/German_admin.php
+++ e107_plugins/tinymce4/languages/German_admin.php
@@ -15,3 +15,5 @@ define("TMCEALAN_3", "Aktiviere dies, wenn im internen Browser Rechtschreibprüf
 define("TMCEALAN_4", "Visuelle Funktionsblöcke");
 define("TMCEALAN_5", "Aktivieren um HTML Funktionsblöcke während der Bearbeitung sichtbar zu machen.");
 define("TMCEALAN_6", "Code-Highlight CSS class.");
+define("TMCEALAN_7", "Frontend-Stil verwenden");
+define("TMCEALAN_8", "Wenn diese Option aktiviert ist, wird der Editor den Stil des Frontend-Themas verwenden. (wenn unterstützt)");
