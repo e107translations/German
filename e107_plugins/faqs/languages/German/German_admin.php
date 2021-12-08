f20c5fa663eedf206b6cf791cd661d86f5f282c6
leo <the_guardian2@hotmail.com>
2021-12-08 19:36:14 +0100

    Update German_admin.php

--- e107_plugins/faqs/languages/German/German_admin.php
+++ e107_plugins/faqs/languages/German/German_admin.php
@@ -1,13 +1,15 @@
 <?php
 
-// Bing-Translated Language file 
+
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
 define("LANA_FAQ_QUESTION", "Frage");
+define("LANA_FAQ_QUESTIONS", "Questions");
 define("LANA_FAQ_ANSWER", "Antwort");
-define("LANA_FAQ_COMMENT", "Kommentar");
-define("LANA_FAQ_UNAME", "Benutzername");
+define("LANA_FAQ_UNANSWERED", "Unanswered");
+define("LANA_FAQ_COMMENT", "Kommentar Klasse");
+//define("LANA_FAQ_UNAME", "Benutzername");
 define("LANA_FAQ_ULOGINNAME", "Benutzer-login");
 define("LANA_FAQ_TAGS", "Markierungen");
 define("LANA_FAQ_TAGS_HELP", "Durch Kommas getrennte Liste tag");
@@ -16,3 +18,34 @@ define("LANA_FAQ_METAK", "Meta-Keywords");
 define("LANA_FAQ_PREF_1", "Zulassen von häufig gestellten Fragen durch Einreichung:");
 define("LANA_FAQ_PREF_2", "'Frage stellen' begrenzt ist");
 define("LANA_FAQ_PREF_3", "Klassisches Layout verwenden");
+
+define("LANA_FAQ_PREF_4", "'Ask a Question' limit per user");
+define("LANA_FAQ_PREF_5", "'Ask a Question' character limit");
+define("LANA_FAQ_PREF_6", "'Ask a Question' limited to");
+define("LANA_FAQ_PREF_7", "Submitted Questions Acknowledgement");
+define("LANA_FAQ_PREF_8", "List Type");
+define("LANA_FAQ_PREF_9", "Unordered List");
+define("LANA_FAQ_PREF_10", "Ordered List");
+define("LANA_FAQ_PREF_11", "Page Title");
+define("LANA_FAQ_PREF_12", "'New' FAQs are no more than");
+define("LANA_FAQ_PREF_13", "Display FAQ total");
+define("LANA_FAQ_PREF_14", "Display Datestamp");
+define("LANA_FAQ_PREF_15", "Display Social buttons");
+define("LANA_FAQ_PREF_16", "Specified Order");
+define("LANA_FAQ_PREF_17", "All Languages");
+define("LANA_FAQ_PREF_18", "Ascending by ID");
+define("LANA_FAQ_PREF_19", "Descending by ID");
+define("LANA_FAQ_PREF_20", "Ascending by Date");
+define("LANA_FAQ_PREF_21", "Descending by Date");
+define("LANA_FAQ_PREF_22", "Manage Questions");
+define("LANA_FAQ_PREF_23", "Manage Categories");
+
+define("LANA_FAQ_PREF_HELP_1", "0 = no limit");
+define("LANA_FAQ_PREF_HELP_2", "Leave blank to use default");
+
+define("LANA_FAQ_CRON_1", "Unanswered Questions Report");
+define("LANA_FAQ_CRON_2", "Mails a report of unanswered questions to [x].");
+define("LANA_FAQ_CRON_3", "[x] Unanswered Questions at [y]");
+define("LANA_FAQ_CRON_4", "Please login to your site to review and answer these questions using the link below.");
+define("LANA_FAQ_CRON_5", "The [x] most recent questions are displayed below.");
+define("LANA_FAQ_CRON_6", "[x] Unanswered Question as of [y].");
