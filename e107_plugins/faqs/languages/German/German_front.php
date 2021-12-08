fabd8d867b424bb4ae43f3a5c96b8a44053c598d
leo <the_guardian2@hotmail.com>
2021-12-08 19:54:42 +0100

    Update German_front.php

--- e107_plugins/faqs/languages/German/German_front.php
+++ e107_plugins/faqs/languages/German/German_front.php
@@ -1,10 +1,21 @@
 <?php
 
-// Bing-Translated Language file 
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("LAN_FAQS_TAGS", "Markierungen");
-define("LAN_FAQS_FILTER_ACTIVE", "Aktive Filter");
-define("LAN_FAQS_NONE_AVAILABLE", "Es gibt zur Zeit keine FAQs.");
-define("LAN_FAQS_ASKQUESTION_AFTER", "Danke. Ihre Frage wurde gespeichert und wird so schnell wie möglich beantwortet.");
+define("LAN_FAQS_001", "Markierungen");
+define("LAN_FAQS_002", "Filter");
+define("LAN_FAQS_003", "Es gibt zur Zeit keine FAQs.");
+define("LAN_FAQS_004", "Danke. Ihre Frage wurde gespeichert und wird so schnell wie möglich beantwortet.");
+define("LAN_FAQS_005", "Sie haben die maximale Anzahl an neuen Fragen erreicht. Sie können weitere Fragen stellen, sobald Ihre bestehenden Fragen beantwortet wurden.");
+define("LAN_FAQS_006", "Entfernen");
+define("LAN_FAQS_007", "Reset");
+define("LAN_FAQS_008", "[x]: Nicht in den Suchergebnissen gefunden.");
+define("LAN_FAQS_009", "moderate Kommentare");
+define("LAN_FAQS_010", "Zurück zu Kategorien");
+define("LAN_FAQS_011", "FAQ");
+define("LAN_FAQS_012", "Geben Sie hier Ihre Frage ein...");
+define("LAN_FAQS_013", "Zeichenbeschränkung");
+define("LAN_FAQS_014", "Bitte [registrieren] und/oder anmelden, um eine Frage zu stellen.");
+define("LAN_FAQS_015", "Nicht erlaubt.");
+define("LAN_FAQS_016", "Sie haben um Antworten auf die folgenden Fragen gebeten:");
