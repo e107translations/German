6b54479db55149353d2168c7aeb7024480cbcab2
leo <the_guardian2@hotmail.com>
2021-12-08 20:14:53 +0100

    Update German_global.php

--- e107_plugins/forum/languages/German/German_global.php
+++ e107_plugins/forum/languages/German/German_global.php
@@ -20,6 +20,3 @@ define("FORUM_LAN_URL_DEFAULT_LABEL", "Standard Forum URLs");
 define("FORUM_LAN_URL_DEFAULT_DESCR", "URLs von ' GET Typ ' ohne zentrale Anlaufstelle . Beispiele: <br /> http://ihreseite.com/e107_plugins/forum/forum.php (forum index) <br /> http://ihreseite.com/e107_plugins/forum/forum_viewtopic.php?id=2 (Thema Ansicht)");
 define("FORUM_LAN_URL_REWRITE_LABEL", "SEF URLs Forum (in Entwicklung)");
 define("FORUM_LAN_URL_REWRITE_DESCR", "Beispiel:<br/>(in Entwicklung)");
-
-
-?>
