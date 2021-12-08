a3f6655d12cc778beb83d06572249dc776a52537
leo <the_guardian2@hotmail.com>
2021-12-08 19:47:26 +0100

    Update German_admin.php

--- e107_plugins/faqs/languages/German/German_admin.php
+++ e107_plugins/faqs/languages/German/German_admin.php
@@ -5,9 +5,9 @@
 // https://github.com/e107inc/multilan
 
 define("LANA_FAQ_QUESTION", "Frage");
-define("LANA_FAQ_QUESTIONS", "Questions");
+define("LANA_FAQ_QUESTIONS", "Fragen");
 define("LANA_FAQ_ANSWER", "Antwort");
-define("LANA_FAQ_UNANSWERED", "Unanswered");
+define("LANA_FAQ_UNANSWERED", "Nicht beantwortet");
 define("LANA_FAQ_COMMENT", "Kommentar Klasse");
 //define("LANA_FAQ_UNAME", "Benutzername");
 define("LANA_FAQ_ULOGINNAME", "Benutzer-login");
@@ -19,33 +19,33 @@ define("LANA_FAQ_PREF_1", "Zulassen von häufig gestellten Fragen durch Einreich
 define("LANA_FAQ_PREF_2", "'Frage stellen' begrenzt ist");
 define("LANA_FAQ_PREF_3", "Klassisches Layout verwenden");
 
-define("LANA_FAQ_PREF_4", "'Ask a Question' limit per user");
-define("LANA_FAQ_PREF_5", "'Ask a Question' character limit");
-define("LANA_FAQ_PREF_6", "'Ask a Question' limited to");
-define("LANA_FAQ_PREF_7", "Submitted Questions Acknowledgement");
-define("LANA_FAQ_PREF_8", "List Type");
-define("LANA_FAQ_PREF_9", "Unordered List");
-define("LANA_FAQ_PREF_10", "Ordered List");
-define("LANA_FAQ_PREF_11", "Page Title");
-define("LANA_FAQ_PREF_12", "'New' FAQs are no more than");
-define("LANA_FAQ_PREF_13", "Display FAQ total");
-define("LANA_FAQ_PREF_14", "Display Datestamp");
-define("LANA_FAQ_PREF_15", "Display Social buttons");
-define("LANA_FAQ_PREF_16", "Specified Order");
-define("LANA_FAQ_PREF_17", "All Languages");
-define("LANA_FAQ_PREF_18", "Ascending by ID");
-define("LANA_FAQ_PREF_19", "Descending by ID");
-define("LANA_FAQ_PREF_20", "Ascending by Date");
-define("LANA_FAQ_PREF_21", "Descending by Date");
-define("LANA_FAQ_PREF_22", "Manage Questions");
-define("LANA_FAQ_PREF_23", "Manage Categories");
+define("LANA_FAQ_PREF_4", "'Frage stellen'-Limit pro Benutzer");
+define("LANA_FAQ_PREF_5", "'Zeichenbegrenzung für 'Frage stellen");
+define("LANA_FAQ_PREF_6", "'Eine Frage stellen' beschränkt auf");
+define("LANA_FAQ_PREF_7", "Eingereichte Fragen Danksagung");
+define("LANA_FAQ_PREF_8", "Liste Type");
+define("LANA_FAQ_PREF_9", "Unsortierte Liste");
+define("LANA_FAQ_PREF_10", "Sortierte Liste");
+define("LANA_FAQ_PREF_11", "Titel der Seite");
+define("LANA_FAQ_PREF_12", "'Neue' FAQs sind nicht mehr als");
+define("LANA_FAQ_PREF_13", "FAQ insgesamt anzeigen");
+define("LANA_FAQ_PREF_14", "Datumsstempel anzeigen");
+define("LANA_FAQ_PREF_15", "Soziale Schaltflächen anzeigen");
+define("LANA_FAQ_PREF_16", "angegebene Reihenfolge");
+define("LANA_FAQ_PREF_17", "Alle Sprachen");
+define("LANA_FAQ_PREF_18", "Aufsteigend nach ID");
+define("LANA_FAQ_PREF_19", "Absteigend nach ID");
+define("LANA_FAQ_PREF_20", "Aufsteigend nach Datum");
+define("LANA_FAQ_PREF_21", "Absteigend nach Datum");
+define("LANA_FAQ_PREF_22", "Verwalten von Fragen");
+define("LANA_FAQ_PREF_23", "Verwalten von Kategorien");
 
-define("LANA_FAQ_PREF_HELP_1", "0 = no limit");
-define("LANA_FAQ_PREF_HELP_2", "Leave blank to use default");
+define("LANA_FAQ_PREF_HELP_1", "0 = kein Limit");
+define("LANA_FAQ_PREF_HELP_2", "Leer lassen, um Standard zu verwenden");
 
-define("LANA_FAQ_CRON_1", "Unanswered Questions Report");
-define("LANA_FAQ_CRON_2", "Mails a report of unanswered questions to [x].");
-define("LANA_FAQ_CRON_3", "[x] Unanswered Questions at [y]");
-define("LANA_FAQ_CRON_4", "Please login to your site to review and answer these questions using the link below.");
-define("LANA_FAQ_CRON_5", "The [x] most recent questions are displayed below.");
-define("LANA_FAQ_CRON_6", "[x] Unanswered Question as of [y].");
+define("LANA_FAQ_CRON_1", "Unbeantwortete Fragen Bericht");
+define("LANA_FAQ_CRON_2", "Sendet einen Bericht über unbeantwortete Fragen an [x].");
+define("LANA_FAQ_CRON_3", "[x] Unbeantwortete Fragen bei [y]");
+define("LANA_FAQ_CRON_4", "Bitte loggen Sie sich auf Ihrer Website ein, um diese Fragen über den unten stehenden Link zu überprüfen und zu beantworten.");
+define("LANA_FAQ_CRON_5", "Die [x] neuesten Fragen werden unten angezeigt.");
+define("LANA_FAQ_CRON_6", "[x] Unbeantwortete Frage ab [y].");
