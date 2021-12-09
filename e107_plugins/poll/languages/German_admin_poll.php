dfbb7762efd8e7eb6b6b97e0e9065824eceb5e11
leo <the_guardian2@hotmail.com>
2021-12-09 13:26:29 +0100

    Update German_admin_poll.php

--- e107_plugins/poll/languages/German_admin_poll.php
+++ e107_plugins/poll/languages/German_admin_poll.php
@@ -11,18 +11,15 @@
 |        $Author: admin $
 +---------------------------------------------------------------+
 */
-define("POLL_ADLAN03", "Umfragen konfigurieren");
+//define("POLL_ADLAN03", "Umfragen konfigurieren");
 define("POLL_ADLAN04", "Das Umfrageplugin wurde erfolgreich installiert. Gehen Sie in Ihren Menübereich um es zu aktivieren, und klicken Sie im Pluginbereich Ihrer Administration auf das Umfrage Ikon um das Plugin zu konfigurieren und/oder neue Umfragen zu schalten.");
-define("POLL_ADLAN05", "Haupt - Umfrage");
-define("POLL_ADLAN06", "Forum Thema:");
-define("POLL_ADLAN07", "Typ");
-define("POLLAN_MENU_CAPTION", "Umfrage");
-define("POLLAN_7", "Keine Umfragen bis jetzt.");
-define("LAN_AL_POLL_01", "Umfrage gelöscht");
+//define("POLL_ADLAN05", "Haupt - Umfrage");
+//define("POLL_ADLAN06", "Forum Thema:");
+//define("POLL_ADLAN07", "Typ");
+//define("POLLAN_MENU_CAPTION", "Umfrage");
+//define("POLLAN_7", "Keine Umfragen bis jetzt.");
+//define("LAN_AL_POLL_01", "Umfrage gelöscht");
 define("LAN_AL_POLL_02", "Umfrage aktualisiert");
 define("LAN_AL_POLL_03", "Umfrage hinzugefügt");
 define("LAN_AL_POLL_04", "");
 define("LAN_AL_POLL_05", "");
-
-
-?>
